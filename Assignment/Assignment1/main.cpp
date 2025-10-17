e6e7e457f12872732dcd0c8b6df73ef3bda75276
ArisuMika <126974855+ArisuMika520@users.noreply.github.com>
2025-10-18 02:48:47 +0800

    作业1——完成

--- Assignment/Assignment1/main.cpp
+++ Assignment/Assignment1/main.cpp
@@ -23,9 +23,39 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
-    // Create the model matrix for rotating the triangle around the Z axis.
-    // Then return it.
+    float angle_red = rotation_angle * MY_PI / 180.0f;
+
+    model << std::cos(angle_red), -std::sin(angle_red), 0, 0,
+        std::sin(angle_red), std::cos(angle_red), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+    return model;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle) 
+{
+    // 1. 角度转弧度
+    float angle_rad = angle * MY_PI / 180.0f;
+
+    // 2. 轴向量单位化
+    Vector3f n = axis.normalized();
+
+    // 3. 构建 3x3 旋转矩阵 R
+    Eigen::Matrix3f R;
+    Eigen::Matrix3f I = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f N; // 叉积矩阵
+    N << 0, -n.z(), n.y(),
+        n.z(), 0, -n.x(),
+        -n.y(), n.x(), 0;
+
+    R = std::cos(angle_rad) * I +
+        (1 - std::cos(angle_rad)) * n * n.transpose() +
+        std::sin(angle_rad) * N;
+
+    // 4. 将 3x3 旋转矩阵扩展为 4x4 变换矩阵
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+    model.block<3, 3>(0, 0) = R;
 
     return model;
 }
@@ -36,10 +66,28 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // Students will implement this function
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    // 将垂直视场角转换为弧度
+    float fov_rad = eye_fov * MY_PI / 180.0f;
 
-    // TODO: Implement this function
-    // Create the projection matrix for the given parameters.
-    // Then return it.
+    float t = std::tan(fov_rad / 2.0f) * std::abs(zNear);
+    float r = t * aspect_ratio;
+
+    // 构建透视-正交变换矩阵
+    Eigen::Matrix4f persp_to_ortho;
+    persp_to_ortho << zNear, 0, 0, 0,
+        0, zNear, 0, 0,
+        0, 0, zNear + zFar, -zNear * zFar,
+        0, 0, 1, 0;
+
+    // 构建正交投影矩阵
+    Eigen::Matrix4f ortho;
+    ortho << 1.0f / r, 0, 0, 0,
+        0, 1.0f / t, 0, 0,
+        0, 0, 2.0f / (zNear - zFar), 0,
+        0, 0, 0, 1;
+
+    // 最终的投影矩阵是两者的乘积
+    projection = ortho * persp_to_ortho;
 
     return projection;
 }
@@ -91,7 +139,7 @@ int main(int argc, const char** argv)
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(Eigen::Vector3f(0, 1, 0),angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
